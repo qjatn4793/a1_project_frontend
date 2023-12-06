557e7468146bc3264e40e649175f4539a3b47017
김범수 <qjatn4793@naver.com>
2023-12-06 14:13:33 +0900

    [2023-12-06] 부트스트랩 템플릿 적용 및 일부 커스터마이징

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*!
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+=========================================================
+* Now UI Dashboard React - v1.5.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/now-ui-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/now-ui-dashboard-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/scss/now-ui-dashboard.scss?v1.5.0";
+import "assets/css/demo.css";
+
+import AdminLayout from "layouts/Admin.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<AdminLayout />} />
+      <Route path="*" element={<Navigate to="/dashboard" replace />} />
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/views/Dashboard.js
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Table,
+  Button,
+  Form,
+} from "reactstrap";
+
+function Dashboard() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+  const [yourData, setYourData] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [itemDetails, setItemDetails] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (selectedItem) {
+      fetchItemDetails(selectedItem);
+    }
+  }, [selectedItem]);
+
+  const fetchData = () => {
+    axios.get("http://localhost:8080/api/search")
+      .then((response) => {
+        setYourData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
+  const fetchItemDetails = (selectedItem) => {
+    axios.get(`http://localhost:8080/api/searchItem?item=${selectedItem}`)
+      .then((response) => {
+        setItemDetails(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching item details:", error);
+      });
+  };
+
+  const handleSearch = () => {
+    // 검색어가 비어 있으면 검색을 수행하지 않음
+    if (searchTerm.trim() === "") {
+      setFilteredData([]);
+      return;
+    }
+
+    fetchData();
+
+    const filteredResults = yourData.filter((item) =>
+      item.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setFilteredData(filteredResults);
+  };
+
+  const handleResultClick = (item) => {
+    setSelectedItem(item);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleSearch();
+  };
+
+  return (
+    <>
+      <div className="content d-flex justify-content-center align-items-center">
+        <div>
+          <h1 className="d-flex justify-content-center align-items-center">A1</h1>
+          <Form onSubmit={handleSubmit}>
+            <InputGroup className="no-border" style={{ width: '500px' }}>
+              <Input
+                placeholder="Search..."
+                style={{ backgroundColor: 'white' }}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+              <InputGroupAddon addonType="append">
+                <InputGroupText
+                  style={{ backgroundColor: 'white', cursor: 'pointer' }}
+                  onClick={handleSearch}
+                >
+                  <i
+                    className="now-ui-icons ui-1_zoom-bold"
+                    style={{ transition: 'color 0.3s' }}
+                  />
+                </InputGroupText>
+              </InputGroupAddon>
+            </InputGroup>
+          </Form>
+          <Table>
+            <thead>
+              <tr>
+                <th>Results</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredData.map((item, index) => (
+                <tr key={index} onClick={() => handleResultClick(item)}>
+                  <td>{item}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          {itemDetails && (
+            <div>
+              <h2>Item Details</h2>
+              {/* Display itemDetails here */}
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
